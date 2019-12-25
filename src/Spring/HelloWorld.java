98f08cb360869abb22d68218e2228f392fc1e351
Tom <404526222@qq.com>
2019-12-25 11:12:14 +0800

    Test

--- src/Spring/HelloWorld.java
+++ src/Spring/HelloWorld.java
@@ -1,3 +1,4 @@
+package Spring;
 
 public class HelloWorld {
 
---
+++ src/Spring/Main.java
@@ -0,0 +1,12 @@
+package Spring;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		HelloWorld helloWorld = (HelloWorld)context.getBean("helloWorld");
+		helloWorld.say();
+	}
+}
